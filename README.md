4dd0cd105db664738194c2f93b325b82525624a0
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-03-12 18:33:22 +0100

    Add preliminary orthographic profile (under development)

--- README.md
+++ README.md
@@ -18,6 +18,8 @@ Conceptlists in Concepticon:
 ![Glottolog: 98%](https://img.shields.io/badge/Glottolog-98%25-green.svg "Glottolog: 98%")
 ![Concepticon: 100%](https://img.shields.io/badge/Concepticon-100%25-brightgreen.svg "Concepticon: 100%")
 ![Source: 95%](https://img.shields.io/badge/Source-95%25-green.svg "Source: 95%")
+![BIPA: 47%](https://img.shields.io/badge/BIPA-47%25-red.svg "BIPA: 47%")
+![CLTS SoundClass: 47%](https://img.shields.io/badge/CLTS%20SoundClass-47%25-red.svg "CLTS SoundClass: 47%")
 
 - **Varieties:** 1,632
 - **Concepts:** 210
@@ -26,9 +28,21 @@ Conceptlists in Concepticon:
 - **Synonymy:** 1.14
 - **Cognacy:** 233,260 cognates in 17,920 cognate sets (439 singletons)
 - **Cognate Diversity:** 0.06
+- **Invalid lexemes:** 0
+- **Tokens:** 1,659,975
+- **Segments:** 417 (222 BIPA errors, 222 CTLS sound class errors, 195 CLTS modified)
+- **Inventory size (avg):** 28.32
 
 ## Possible Improvements:
 
 
 
 - Entries missing sources: 14138/313273 (4.51%)
+
+# Contributors
+
+Name | GitHub user | Role
+--- | --- | ---
+Simon Greenhill | @SimonGreenhill | maintainer
+
+
--- lexibank_abvd.py
+++ lexibank_abvd.py
@@ -3,7 +3,9 @@ import re
 from pathlib import Path
 
 from clldutils.misc import slug
-from pybtex.database import parse_string  # dependency of pycldf, so should be installed.
+from pybtex.database import (
+    parse_string,
+)  # dependency of pycldf, so should be installed.
 from pylexibank.providers import abvd
 from pylexibank.util import progressbar
 from pylexibank import FormSpec
@@ -11,27 +13,25 @@ from pylexibank import FormSpec
 
 class Dataset(abvd.BVD):
     dir = Path(__file__).parent
-    id = 'abvd'
-    SECTION = 'austronesian'
-    
-    invalid_ids = [
-        261,  # Duplicate West Futuna list
-    ]
-    
+    id = "abvd"
+    SECTION = "austronesian"
+
+    invalid_ids = [261]  # Duplicate West Futuna list
+
     max_language_id = 2000
 
     form_spec = FormSpec(
         brackets={"[": "]", "{": "}", "(": ")"},
         separators=";/,~",
-        missing_data=('-', ),
+        missing_data=("-",),
         strip_inside_brackets=True,
     )
-    
+
     def cmd_makecldf(self, args):
         args.writer.add_sources(*self.etc_dir.read_bib())
         concepts = args.writer.add_concepts(
-            id_factory=lambda c: c.id.split('-')[-1]+ '_' + slug(c.english),
-            lookup_factory=lambda c: c['ID'].split('_')[0]
+            id_factory=lambda c: c.id.split("-")[-1] + "_" + slug(c.english),
+            lookup_factory=lambda c: c["ID"].split("_")[0],
         )
         for wl in progressbar(self.iter_wordlists(args.log), desc="cldfify"):
             wl.to_cldf(args.writer, concepts)
